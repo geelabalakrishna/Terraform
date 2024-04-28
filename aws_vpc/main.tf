ad5be3e39f6d8b8079f126265ca6d88ed50e95d9
balakrishnag <balakrishna.cloud08@gmail.com>
2024-04-28 18:25:58 +0530

    aws_vpc_mod

--- aws_vpc/main.tf
+++ aws_vpc/main.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc" "my_vpc" {
     cidr_block = "10.10.0.0/16"
     instance_tenancy = "default"
-    enable_dns_hostnames = true
+    enable_dns_hostnames = false
     
     tags = {
       Name = "main"
